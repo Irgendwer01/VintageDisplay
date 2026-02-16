1b9ee18c823131bd6dc5ccc936fa66abd867f9ff
Irgendwer <wippichdpaul@gmail.com>
2026-02-16 14:56:41 +0100

    Spotless

--- src/main/java/com/caedis/duradisplay/overlay/OverlayMagic.java
+++ src/main/java/com/caedis/duradisplay/overlay/OverlayMagic.java
@@ -1,11 +1,14 @@
 package com.caedis.duradisplay.overlay;
 
+import net.minecraft.item.ItemStack;
+
+import org.jetbrains.annotations.NotNull;
+
 import com.caedis.duradisplay.config.ConfigDurabilityLike;
 import com.caedis.duradisplay.utils.ColorType;
 import com.caedis.duradisplay.utils.DurabilityFormatter;
 import com.caedis.duradisplay.utils.DurabilityLikeInfo;
-import net.minecraft.item.ItemStack;
-import org.jetbrains.annotations.NotNull;
+
 import vazkii.botania.api.mana.IManaItem;
 
 // Overlay for anything magic alike like Botania Mana
@@ -14,33 +17,33 @@ public class OverlayMagic extends OverlayDurabilityLike {
 
     public OverlayMagic() {
         super(
-            new ConfigDurabilityLike(
-                true,
-                OverlayDurabilityLike.Style.Text,
-                DurabilityFormatter.Format.percent,
-                2,
-                false,
-                true,
-                0x00FF00,
-                ColorType.RYGDurability,
-                new double[] { 30, 70 },
-                new int[] { 0xFF0000, 0x55FF00, 0x00FF00 },
-                true,
-                0,
-                true) {
+                new ConfigDurabilityLike(
+                        true,
+                        OverlayDurabilityLike.Style.Text,
+                        DurabilityFormatter.Format.percent,
+                        2,
+                        false,
+                        true,
+                        0x00FF00,
+                        ColorType.RYGDurability,
+                        new double[] { 30, 70 },
+                        new int[] { 0xFF0000, 0x55FF00, 0x00FF00 },
+                        true,
+                        0,
+                        true) {
 
-                @Override
-                public void postLoadConfig() {
-                    configCategory.setComment("""
+                    @Override
+                    public void postLoadConfig() {
+                        configCategory.setComment("""
                                 Magic is the module that shows magical charge (or whatever) of items
                                                                              """);
-                }
+                    }
 
-                @Override
-                public @NotNull String category() {
-                    return "magic";
-                }
-            });
+                    @Override
+                    public @NotNull String category() {
+                        return "magic";
+                    }
+                });
         addHandler("vazkii.botania.api.mana.IManaItem", OverlayMagic::handleBotania);
     }
 
